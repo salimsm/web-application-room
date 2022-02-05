22565a250bc5dfca7b5544d3f74c8a5f8dfd20c4
salim <salimmaharjansm2@gmail.com>
2022-02-05 14:40:49 +0545

    linked between index, register and login page

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-#colab
 
-#https://pastebin.com/NT7chJGZ
+from flask import Flask,render_template,request
+from flask_mysqldb import MySQL
 
-app=if_name_
+app=Flask(__name__)
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+
+
+
+@app.route('/login')
+def register():
+	return render_template('login.html')
+
+
+@app.route('/register')
+def login():
+	return render_template('register.html')
+
+app.run()
